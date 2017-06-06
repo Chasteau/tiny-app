c41e5717751fa852a4830a9f39422d6b3b812d8e
Chasteau <davidchasteau@gmail.com>
2017-06-06 23:08:19 +0000

    slowly working through project

---
+++ express_server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn3" : "http://www.lighthouselabs.ca",
+   "b2xVn2" : "http://www.google.com"
+ };
+
+app.get("/urls", (request, response) => {
+  let templateVars = { urls: urlDatabase};
+  response.render("urls_index", templateVars);
+})
+
+app.get("/urls/:id", (request, response) => {
+  let templateVars = {shortURl: req.params.id};
+  response.render("urls_show", templateVars);
+});
+
+app.listen(PORT, () =>{
+  console.log(`Examine app listening on port ${PORT}!`)
+})
