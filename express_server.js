9fe8d410cdf2493d990212e0eb425276f311e241
Chasteau <davidchasteau@gmail.com>
2017-06-06 23:43:54 +0000

    more functions working

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -14,10 +16,25 @@ app.get("/urls", (request, response) => {
   response.render("urls_index", templateVars);
 })
 
+app.get("/urls/new", (request, response) => {
+  response.render("urls_new");
+});
+
 app.get("/urls/:id", (request, response) => {
-  let templateVars = {shortURl: request.params.id, longURl: urlDatabase[request.params.id]  };
+  let templateVars = {shortURl: request.params.id,
+    longURl: urlDatabase[request.params.id] };
   response.render("urls_show", templateVars);
 });
+
+app.post("/urls", (request, response) => {
+  console.log(generateRandomString());
+  response.send("Ok");
+});
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2,6);
+}
+
 
 app.listen(PORT, () =>{
   console.log(`Examine app listening on port ${PORT}!`)
