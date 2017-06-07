d74e04c5c6cc877a9e8a2f4c95a8fca2af1d018b
Chasteau <davidchasteau@gmail.com>
2017-06-07 16:45:01 +0000

    refactored urlsDB into an array, broke everything

--- express_server.js
+++ express_server.js
@@ -6,38 +6,46 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-var urlDatabase = {
-  "b2xVn3" : "http://www.lighthouselabs.ca",
-   "b2xVn2" : "http://www.google.com"
- };
+let urlsDB = [
+  {
+    shorter: "b2xVn3",
+    original: "http://www.lighthouselabs.ca"
+  },
+  {
+    shorter: "b2xVn2",
+    original: "http://www.google.com"
+  }
+];
+
+ // function generateRandomString() {
+ //   return Math.random().toString(36).substr(2,6);
+ // }
 
 app.get("/urls", (request, response) => {
-  let templateVars = { urls: urlDatabase};
-  response.render("urls_index", templateVars);
+  let templateVars = {urls: urlsDB};
+  response.render("urls_index", templateVars)
 })
-
-app.get("/urls/new", (request, response) => {
-  response.render("urls_new");
-});
-
-app.get("/urls/:id", (request, response) => {
-  let templateVars = {shortURl: request.params.id,
-    longURl: urlDatabase[request.params.id] };
-  response.render("urls_show", templateVars);
-});
-
-app.get("/u/:shortURL", (request, response) => {
-   let longURL = urlDatabase[request.params.shortURL];
-  response.redirect(longURL);
-});
-
-app.post("/urls", (request, response) => {
-  urlDatabase[generateRandomString()] = request.body.longURL;
-  response.redirect(301, "http://localhost:8080/urls/")});
-
-function generateRandomString() {
-  return Math.random().toString(36).substr(2,6);
-}
+//
+// app.get("/urls/new", (request, response) => {
+//   response.render("urls_new");
+// })
+//
+// app.get("/urls/:id", (request, response) => {
+//   let templateVars = {shortURl: request.params.id,
+//     longURl: urlsDB.original[request.params.id] };
+//   response.render("urls_show", templateVars);
+// })
+//
+// app.get("/u/:shortURL", (request, response) => {
+//    let longURL = urlsDB.original[request.params.shortURL];
+//   response.redirect(longURL);
+// })
+//
+// app.post("/urls", (request, response) => {
+//   urlsDB[shorter] = generateRandomString();
+//   urlsDB[original] = request.body.longURL;
+//   response.redirect(301, "http://localhost:8080/urls/")
+// });
 
 app.listen(PORT, () =>{
   console.log(`Examine app listening on port ${PORT}!`)
