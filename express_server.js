06db6f88bda9f450bb2ce2f088a6293a7b995090
Chasteau <davidchasteau@gmail.com>
2017-06-07 17:50:06 +0000

    delete button working

--- express_server.js
+++ express_server.js
@@ -17,36 +17,89 @@ let urlsDB = [
   }
 ];
 
- // function generateRandomString() {
- //   return Math.random().toString(36).substr(2,6);
- // }
+ function generateRandomString() {
+   return Math.random().toString(36).substr(2,6);
+ }
+
+
+ function findURL(name){
+   let foundURL;
+   urlsDB.forEach((url) => {
+     if(url.shorter == name) {
+       foundURL = url;
+     }
+   });
+   return foundURL;
+ }
 
 app.get("/urls", (request, response) => {
   let templateVars = {urls: urlsDB};
   response.render("urls_index", templateVars)
 })
-//
-// app.get("/urls/new", (request, response) => {
-//   response.render("urls_new");
-// })
-//
-// app.get("/urls/:id", (request, response) => {
-//   let templateVars = {shortURl: request.params.id,
-//     longURl: urlsDB.original[request.params.id] };
+
+app.get("/urls/new", (request, response) => {
+  response.render("urls_new");
+})
+
+app.get("/urls/:id", (request, response) => {
+  // find url from db
+  let url = findURL(request.params.id);
+
+  if(!url) {
+    response.status(404).send("Url Not Found");
+    return;
+  }
+  // update name on output
+  url.original = request.body.original;
+
+  const newDB = urlsDB.map((u) => {
+    if(u.shorter == url.shorter){
+      return url;
+    }
+    return u;
+  });
+
+  urlsDB = newDB;
+
+  response.redirect('/urls/' + reqquest.params.id);
+
+//   let templateVars = {shortURL: request.params.id,
+//     longURL: urlsDB.original[request.params.id] };
 //   response.render("urls_show", templateVars);
 // })
 //
 // app.get("/u/:shortURL", (request, response) => {
 //    let longURL = urlsDB.original[request.params.shortURL];
 //   response.redirect(longURL);
-// })
-//
-// app.post("/urls", (request, response) => {
-//   urlsDB[shorter] = generateRandomString();
-//   urlsDB[original] = request.body.longURL;
-//   response.redirect(301, "http://localhost:8080/urls/")
-// });
+})
+
+app.post("/urls", (request, response) => {
+  const newUrl = {
+      shorter: generateRandomString(),
+      original: request.body.longURL
+  }
+  urlsDB.push(newUrl);
+  console.log(urlsDB);
+  response.redirect(301, "/urls")
+});
+
+app.post('/urls/:id/delete', (request, response) => {
+  // find if url is in db
+  const url = findURL(request.params.id);
+  console.log(url);
+  // if not found return a 404
+  if(!url) {
+    response.status(404).send(" URL not found!");
+    return;
+  }
+  //remove from urlsDB
+  const index = urlsDB.indexOf(url);
+  urlsDB.splice(index, 1);
+
+  //redirect to "/urls" index
+  response.redirect(302,"/urls" )
+})
 
 app.listen(PORT, () =>{
-  console.log(`Examine app listening on port ${PORT}!`)
+  console.log(`TinyApp is listening on port ${PORT}!`)
 })
