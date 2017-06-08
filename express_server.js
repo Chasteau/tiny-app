41b4edb9f5fe6c09c7e07f6821f6299c7cedf85d
Chasteau <davidchasteau@gmail.com>
2017-06-08 22:51:53 +0000

    added login and register functionality

--- express_server.js
+++ express_server.js
@@ -56,35 +56,17 @@ const usersDB = {
       return usersDB[id];
       }
     }
-    return undefined;
+    return null;
  };
 
- // checks if user is in db, and returns user object
+ // checks if user is in db, and returns userID
  function checkUserPass(emailToCheck, passToCheck, cb) {
   let userInDB = cb(emailToCheck);
-    if(userInDB.password === passToCheck) {
+  if (userInDB && userInDB.password === passToCheck) {
       return userInDB.id;
-    } else {
-    return undefined;
     }
- };
-
- // checks if user is in db, and returns user object
- function returnUserID(email, pass) {
-   let userID;
-   if (checkUserEmail(email) && checkUserEmail(pass))
-
-
-   for(var id in usersDB){
-    if((usersDB[id].password === email) &&
-    (){
-      userInDB = true;
-      } else {
-        userInDB = false;
-      }
-    }
-   return userInDB;
- };
+    return false;
+  };
 
 // display urls index page
 app.get("/urls", (request, response) => {
@@ -190,39 +172,29 @@ app.post("/urls/:id/update", (request, response) => {
 
  app.post('/login', (request, response) => {
      // check if email is empty empty string
-     let emailEmpty = validator.isEmpty(request.body.email);
+     let emailEmpty = validator.isEmpty(request.body.username);
      // check if password is empty string
-     let passwordEmpty = validator.isEmpty(request.body.email);
-     // check if user email exists in usersDB
-     let isUserEmail = checkUserEmail(request.body.email);
-     // check if user pass exists in usersDB
-     let isUserPass = checkUserPass(request.body.password);
-     // return userID for email and pass;
-     let useId =
+     let passwordEmpty = validator.isEmpty(request.body.password);
 
      if (emailEmpty || passwordEmpty) {
        return response.redirect(302, "/login")
-     } else if (user && pass){
-       // set cookie "user_id"  and redirect to urls page
-        response.cookie("user_id", requ);
-       return response.redirect(302, "/urls");
-     } return response.redirect(400, "/urls");
-
-
-
-
-   // set cookie using res.cookie without using options.
-   // display username input back to the user
-   response.cookie("username", request.body.username);
-   // redirect user to urls page
-   response.redirect(302, '/urls');
-
+     }
+       //  // return userID for email and pass;
+       //  // checkUserPass(emailToCheck, passToCheck, cb)
+      let userId = checkUserPass(request.body.username,
+        request.body.password, checkUserEmail);
+        if(userId) {
+          // set cookie "user_id" and redirect to urls page
+           response.cookie("user_id", userId);
+           return response.redirect(302, "/urls");
+        }
+      return response.redirect(302, "/login");
  });
 
  // log out users
  app.post('/logout', (request, response) => {
      response.clearCookie("username");
-     response.redirect(302, '/urls');
+     response.redirect(302, '/login');
  });
 
  // user registration
@@ -231,16 +203,17 @@ app.post("/urls/:id/update", (request, response) => {
    let emailEmpty = validator.isEmpty(request.body.email);
    // check if password is empty string
    let passwordEmpty = validator.isEmpty(request.body.email);
-   // check if user exists in usersDB if yes then return, else
-   let user = checkUserEmail(request.body.email);
 
    if (emailEmpty || passwordEmpty) {
-     return response.redirect(400, "/register")
-   } else if (!user){
+     return response.redirect(302, "/register")
+   }
+   // check if user exists in usersDB if yes then return, else
+   let user = checkUserEmail(request.body.email);
+    if (!user){
     // generate random user id using random string function
-    newUserId = generateRandomString();
+      newUserId = generateRandomString();
      //add newuser to usersDB (email, pass, userid)
-     usersDB[newUserId] = {
+      usersDB[newUserId] = {
        Id: newUserId,
        email: request.body.email,
        password: request.body.password
@@ -248,7 +221,7 @@ app.post("/urls/:id/update", (request, response) => {
       // set cookie "user_id"  and redirect to urls page
       response.cookie("user_id", newUserId);
      return response.redirect(302, "/urls");
-   } return response.redirect(400, "/urls");
+   } return response.redirect(302, "/urls");
  });
 
 app.listen(PORT, () =>{
